ac8d4acd9218a018cdb02d25aca994bf2cc0187a
Karol Laskowski <karol@laskowski.dev>
2021-01-31 22:36:37 +0100

    Template for custom Contact page

---
+++ contactpage.php
@@ -0,0 +1,14 @@
+﻿<?php 
+/*
+Template Name: Custom Contact page
+*/
+get_header(); ?>
+
+<main>
+  <div class="container-single">
+    <h1>YOOOOOOOO!!!!</h1>
+    </section>
+  </div>
+</main>
+
+<?php get_footer(); ?>
