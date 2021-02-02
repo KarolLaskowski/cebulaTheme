5833ef7ee6763c427cbdaa27801dbf6a7fd931a7
Karol Laskowski <karol@laskowski.dev>
2021-02-02 11:58:10 +0100

    Adds Contact Page preparations

--- contactpage.php
+++ contactpage.php
@@ -5,9 +5,14 @@ Template Name: Custom Contact page
 get_header(); ?>
 
 <main>
-  <div class="container-single">
-    <h1>YOOOOOOOO!!!!</h1>
+  <div class="container-contact-page">
+    <header>
+      <h1>Nazywam się Karolina Nowak</h1>
+    </header>
+    <section class="contact-column column-images">
+      <img src="http://placekitten.com/g/700/900">
     </section>
+    <section class="contact-column column-text"></section>
   </div>
 </main>
 
