0ef270ad73371f012fd98f9f08f1f098adb55ba5
Karol Laskowski <karol@laskowski.dev>
2021-02-05 12:40:14 +0100

    Adds minimum viable footer

--- footer.php
+++ footer.php
@@ -1,10 +1,34 @@
-<footer id="main-footer">
-<?php wp_footer(); ?>
-  hello
-</footer>
+    <footer id="main-footer">
+      <div class="links">
+        <ul>
+          <li>
+            <a href="<?php bloginfo('url'); ?>">
+              <h4><?php bloginfo('name'); ?></h4>
+            </a>
+          </li>
+          <li><?php bloginfo('description'); ?></li>
+          <li><h5>Site owner<h5></li>
+          <li>site@owner.com</li>
+        </ul>
+        <ul>
+          <li><a href="https://facebook.com/">Facebook</a></li>
+          <li><a href="https://instagram.com/">Instagram</a></li>
+          <li><a href="https://linkedin.com/">LinkedIn</a></li>
+        </ul>
+        <ul>
+          <li>Ciasteczka</li>
+          <li>Tryb nocny</li>
+        </ul>
+      </div>
+      <div class="dev">
+        Theme by Karol L. @ 2021<br>
+        <a href="https://www.codebake.dev/">www.codebake.dev</a>
+      </div>
+    </footer>
 
-</body>
-<script type="text/javascript">
-  MainModule.Init();
-</script>
+  </body>
+  <?php wp_footer(); ?>
+  <script type="text/javascript">
+    MainModule.Init();
+  </script>
 </html>
