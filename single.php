72900a3fb14516be7215d002b9662686dd11a8c6
Karol Laskowski <karol@laskowski.dev>
2021-02-05 12:08:12 +0100

    Adds metadata information under post content

--- single.php
+++ single.php
@@ -23,6 +23,36 @@
         <?php the_content(); ?>
 
       <?php endwhile; endif; ?>
+      
+      <div class="post-meta">
+        <p>Przez <?php the_author(); ?> dnia <?php the_time('F jS, Y'); ?>
+        <?php
+          $u_time = get_the_time('U');
+          $u_modified_time = get_the_modified_time('U');
+          if ($u_modified_time >= $u_time + 86400) {
+            echo " (Aktualizacja ";
+            the_modified_time('F jS, Y');
+            echo ") ";
+          }
+        ?>
+        </p>
+        <p>Kategorie:&nbsp;
+          <?php
+            $i = 0;
+            $categories = get_the_category( $post->ID );
+            foreach ( $categories as $category ) {
+              echo '<span class="wpb-category"><a href="' . get_category_link( $category->term_id ) . '">' .  $category->name  . '</a></span>';
+              if (++$i < count($categories)) {
+                echo "&nbsp;|&nbsp;";
+              }
+            }
+          ?>
+        </p>
+        <p class="float-to-sides">
+          <span><?php previous_post_link('&laquo; %link', 'Poprzedni post', false, '', 'category'); ?></span>
+          <span><?php next_post_link('%link &raquo;', 'Następny post', false, '', 'category'); ?></span>
+        </p>
+      </div>
     </section>
   </div>
 </main>
