322f2c5d87a3b9cb8525bb41697d5e36bf2dd108
Karol Laskowski <karol@laskowski.dev>
2021-02-21 17:08:28 +0100

    Removed not leaded projects from Contact page

--- partial-views/contact-tabs/contact-research-projects.php
+++ partial-views/contact-tabs/contact-research-projects.php
@@ -12,24 +12,3 @@
 <h4>Wpływ metyloparabenu (MeP) na produkcję NO przy udziale iNOS przez ludzkie neutrofile.</h4>
 <p>Projekt finansowany z dotacji MNiSW na działalność statutową dla Uniwersytetu Medycznego w Białymstoku na rok 2017.</p>
 <p>Kierownik projektu: mgr Karolina Nowak</p>
-<h4>Wielopoziomowa ocena działania Bisfenolu A na ludzkie leukocyty.</h4>
-<p>Projekt finansowany z dotacji MNiSW dla Uniwersytetu Medycznego w Białymstoku na rok 2020.</p>
-<p>Kierownik projektu: dr hab. n. med. Wioletta Ratajczak-Wrona</p>
-<h4>Mechanizm regulacji inflamasomu NLRP3 w komórkach raka płaskonabłonkowego szyi przez parabeny.</h4>
-<p>Projekt finansowany z dotacji MNiSW dla Uniwersytetu Medycznego w Białymstoku na rok 2020.</p>
-<p>Kierownik projektu: prof. dr hab. n. med. Ewa Jabłońska</p>
-<h4>Ocena regulacji funkcji i czasu życia neutrofilów eksponowanych na bisfenol A (BPA).</h4>
-<p>Projekt finansowany z dotacji MNiSW na działalność statutową dla Uniwersytetu Medycznego w Białymstoku na rok 2018.</p>
-<p>Kierownik projektu: dr hab. n. med. Wioletta Ratajczak-Wrona</p>
-<h4>Ocena biologicznych markerów działania tlenku azotu w surowicy pacjentów ze złamaniami kości szczęk.</h4>
-<p>Projekt finansowany z dotacji MNiSW na działalność statutową dla Uniwersytetu Medycznego w Białymstoku na rok 2018.</p>
-<p>Kierownik projektu: dr hab. n. med. Wioletta Ratajczak-Wrona</p>
-<h4>Rola szlaku PI3K-AKT/PKB w regulacji produkcji tlenku azotu i ekspresji iNOS przez ludzkie leukocyty eksponowane na bisfenol A.</h4>
-<p>Projekt finansowany z dotacji MNiSW na działalność statutową dla Uniwersytetu Medycznego w Białymstoku na rok 2017.</p>
-<p>Kierownik projektu: prof. dr hab. n. med. Ewa Jabłońska</p>
-<h4>Rola szlaków sygnalizacyjnych w produkcji tlenku azotu przy udziale iNOS w leukocytach narażonych na działanie Bisfenolu A (BPA).</h4>
-<p>Projekt finansowany z dotacji MNiSW na działalność statutową dla Uniwersytetu Medycznego w Białymstoku na rok 2017.</p>
-<p>Kierownik projektu: dr hab. n. med. Wioletta Ratajczak-Wrona</p>
-<h4>Wpływ metyloparabenu (MeP) na wybrane funkcje ludzkich neutrofilów. </h4>
-<p>Projekt finansowany z dotacji MNiSW na działalność statutową dla Uniwersytetu Medycznego w Białymstoku na rok 2017.</p>
-<p>Kierownik projektu: prof. dr hab. Ewa Jabłońska</p>
